cb919853cc4d34157f746a8726656d37b2e0b359
calvinjung <myadiuio@gmail.com>
2021-08-30 15:32:53 +0900

    Update

--- src/js/mainpage.js
+++ src/js/mainpage.js
@@ -1,26 +1,26 @@
-const { electronDialog, getGlobal, getCurrentWindow } = require('electron').remote;
+const { dialog, getGlobal, getCurrentWindow } = require('electron').remote;
 const { ipcRenderer, BrowserWindow } = require("electron");
 const path = require('path');
 
-var currentMenu
-var allMenus = ["Live", "Demo", "Misc"]
+var currentMenu;
+var allMenus = ["Live", "Demo", "Misc"];
 
 let store = getGlobal('store');
 let basedir = store.get('basedir');
 
 function changeActiveMenu(name) {
-    for (a in allMenus) {
+    for (let a in allMenus) {
         if (allMenus[a] === name) {
-            let menuSpan = document.getElementById(`menu${name}`).getElementsByTagName("span")[0]
-            menuSpan.style.setProperty("color", "white")
-            let menu = document.getElementById(`menu${name}`)
-            menu.style.setProperty("border-bottom", "2px solid yellow")
-            $(document.getElementsByClassName("content")).load(`../html/menus/${name.toLowerCase()}.html`)
+            let menuSpan = document.getElementById(`menu${name}`).getElementsByTagName("span")[0];
+            menuSpan.style.setProperty("color", "white");
+            let menu = document.getElementById(`menu${name}`);
+            menu.style.setProperty("border-bottom", "2px solid yellow");
+            $(document.getElementsByClassName("content")).load(`../html/menus/${name.toLowerCase()}.html`);
         } else {
-            let menuSpan = document.getElementById(`menu${allMenus[a]}`).getElementsByTagName("span")[0]
-            menuSpan.style.setProperty("color", "lightgrey")
-            let menu = document.getElementById(`menu${allMenus[a]}`)
-            menu.style.setProperty("border-color", "transparent")
+            let menuSpan = document.getElementById(`menu${allMenus[a]}`).getElementsByTagName("span")[0];
+            menuSpan.style.setProperty("color", "lightgrey");
+            let menu = document.getElementById(`menu${allMenus[a]}`);
+            menu.style.setProperty("border-color", "transparent");
         }
     }
 }
@@ -42,16 +42,16 @@ function minimizeWindow() {
 }
 
 function menuLive() {
-    currentMenu = "live"
-    changeActiveMenu("Live")
+    currentMenu = "live";
+    changeActiveMenu("Live");
 }
 
 function menuDemo() {
-    currentMenu = "demo"
-    changeActiveMenu("Demo")
+    currentMenu = "demo";
+    changeActiveMenu("Demo");
 }
 
 function menuMisc() {
-    currentMenu = "misc"
-    changeActiveMenu("Misc")
+    currentMenu = "misc";
+    changeActiveMenu("Misc");
 }
